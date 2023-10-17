d88a1735fab0ced3806699d649efe15a4101383e
liabueno <juliavbueno13@gmail.com>
2023-10-17 20:56:28 -0300

    .

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -100,8 +100,12 @@ function atualizarTabela(produtos) {
       <td>${produto.preco}</td>
       <td>${produto.desc}</td>
       <td>${produto.tipo}</td>
-      <td><center><button type="button" class="btn btn-secondary funcoes"><img src="./src/image/lapis.png" class="lapis"></button></center></td>
-      <td><center><button type="button" class="btn btn-danger delete-button funcoes" data-product-id="${produto.id}" data-product-name="${produto.nome}"><img src="./src/image/excluir.png" class="excluir"></button></center></td>
+      <td><center><button type="button" class="btn btn-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill lapis" viewBox="0 0 16 16">
+      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+    </svg></button></center></td>
+      <td><center><button type="button" class="btn btn-danger delete-button" data-product-id="${produto.id}" data-product-name="${produto.nome}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill excluir" viewBox="0 0 16 16">
+      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+    </svg></button></center></td>
     `;
     listagens.appendChild(newRow);
   });
@@ -221,6 +225,19 @@ function confirmarExclusao(id, nome) {
 }
 
 
+// async function excluirProduto(id) {
+//   try {
+//     // Use o Firebase Firestore para excluir o produto pelo ID
+//     await deleteDoc(doc(db, 'produtos', id));
+//     Swal.fire('Sucesso!', 'Produto excluído com sucesso!', 'success');
+//     // Atualize a lista de produtos após a exclusão
+//     getProdutos();
+//   } catch (error) {
+//     console.error('Erro ao excluir o produto:', error);
+//     Swal.fire('Erro!', 'Ocorreu um erro ao excluir o produto.', 'error');
+//   }
+// }
+
 async function excluirProduto(id) {
   try {
     // Use o Firebase Firestore para excluir o produto pelo ID
@@ -233,6 +250,7 @@ async function excluirProduto(id) {
     Swal.fire('Erro!', 'Ocorreu um erro ao excluir o produto.', 'error');
   }
 }
+
 
 
 
