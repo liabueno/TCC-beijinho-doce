f5fe925d89c5f96151e7f4e93322ae54b7b0217d
liabueno <juliavbueno13@gmail.com>
2023-11-21 14:14:14 -0300

    .

--- pages/bolos/assets/js/index.js
+++ pages/bolos/assets/js/index.js
@@ -47,8 +47,9 @@ async function getProdutos() {
         }
 
         // FORMATANDO O PESO, CONVERTENDO PARA KG CASO PRECISO
+        
         const pesoFormatado = doc.data().peso > 1000
-        ? `${(doc.data().peso / 1000).toFixed(2)}kg`
+        ? `${(doc.data().peso / 1000)}kg`.replace('.',',')
         : `${doc.data().peso}g`;
  
         // CRIAÇÃO DOS CARDS
